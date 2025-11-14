c56c57ce2b4b8432d19e79cb0b7baaa1c0af6127
Dval05 <andrade.dval@gmail.com>
2025-11-13 19:28:24 -0500

    Remove legacy PHP code

--- PHP/config/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('UPLOAD_DIR', '../uploads/profiles/');
-define('SESSION_TIMEOUT', 3600); // 1 hora
-define('DEFAULT_ROLE', 'USER');
-define('SYSTEM_EMAIL', 'noreply@nicekidscenter.com');
-?>
--- PHP/dashboard/recent_activities.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config/database.php";
-header('Content-Type: application/json');
-$res = $conn->query("SELECT Name, ScheduledDate, Category, Status FROM activity ORDER BY ScheduledDate DESC LIMIT 5");
-$data = [];
-while($row = $res->fetch_assoc()) $data[] = $row;
-echo json_encode(["success"=>true,"data"=>$data]);
-$conn->close();
-?>
